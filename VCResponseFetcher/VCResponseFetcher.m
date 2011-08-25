991717aa9191e0d44e0f3f5cb4f79fa8253493eb
Vinay Chavan <vinaychavan22@gmail.com>
2011-08-25 15:10:56 +0530

    updating

--- VCResponseFetcher/VCResponseFetcher.m
+++ VCResponseFetcher/VCResponseFetcher.m
@@ -3,8 +3,26 @@
 //  Demo
 //
 //  Created by Vinay Chavan on 15/06/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  
+//  Copyright (C) 2011 by Vinay Chavan
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions: 
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import "VCResponseFetcher.h"
 
@@ -48,7 +66,7 @@
 				url:(NSString*)url
 			  cache:(NSURLRequestCachePolicy)cache
   responseProcessor:(NSObject<VCDataProcessorDelegate>*)processor
-{	
+{
 	VCResponseFetchService *operation = [[[VCResponseFetchService alloc] init] autorelease];
 	operation.delegate = observer;
 	operation.url = url;
